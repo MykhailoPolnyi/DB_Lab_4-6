8267c1dbc5c2658f8a26f8127f1de196995e2178
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-11-29 01:36:11 +0200

    Added swagger for task

--- src/main/java/ua/lviv/iot/App.java
+++ src/main/java/ua/lviv/iot/App.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-    public Integer a;
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
